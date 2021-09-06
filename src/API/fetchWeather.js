2891bf23730b2a44b2a05fd83138b86dabfdc578
shubham khulbe <skhulbe75@gmail.com>
2021-09-06 21:09:55 +0530

    first commit

---
+++ src/API/fetchWeather.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+
+const URL = 'https://api.openweathermap.org/data/2.5/weather'
+const API_KEY = '3e9b1e916bdd92c299d6ea4a65b80c02'
+
+export const fetchWeather = async (query) =>{
+    const {data} = await axios.get(URL, {
+        params:{
+            q: query,
+            units: 'metrics',
+            APPID: API_KEY
+        }
+    });
+   return data;
+}
